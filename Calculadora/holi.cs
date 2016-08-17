13a0be287f3fd6a50d7a2ddb0978fe470d1ff8db
panchito <francisco.verac@alumnos.uv.cl>
2016-08-17 11:56:33 -0300

    panchito

---
+++ Calculadora/holi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class holi
+    {
+       public int suma(int a,int b){
+          
+          return  a + b;
+        }
+
+        public int resta(int a, int b){
+            return a - b;
+        }
+        public int multipicar(int a, int b)
+        {
+            return a * b;
+        }
+        public int dividir(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
